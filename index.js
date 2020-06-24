abf4744cb521a69d00b9a343463c0317e84b7b84
devo-ody <rodrigo@raising.solutions>
2020-06-24 05:18:54 -0300

    + V1 is Ready to be tested: it seems able to deal with any text content, however it is not able to get the message body from context by itself

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const crypto = require('crypto');
+const jsSHA = require('./jsSHA');
+
 
 module.exports.templateTags = [
   {
@@ -10,10 +12,9 @@ module.exports.templateTags = [
         displayName: 'Hashing Algorithm',
         type: 'enum',
         options: [
-          { displayName: 'MD5', value: 'md5' },
-          { displayName: 'SHA1', value: 'sha1' },
-          { displayName: 'SHA256', value: 'sha256' },
-          { displayName: 'SHA512', value: 'sha512' },
+          { displayName: 'SHA-1', value: 'SHA-1' },
+          { displayName: 'SHA-256', value: 'SHA-256' },
+          { displayName: 'SHA-512', value: 'SHA-512' },
         ],
       },
       {
@@ -21,8 +22,9 @@ module.exports.templateTags = [
         description: 'The encoding of the content hash generated',
         type: 'enum',
         options: [
-          { displayName: 'Hexadecimal', value: 'hex' },
-          { displayName: 'Base64', value: 'base64' },
+          { displayName: 'Base64', value: 'B64' },
+          { displayName: 'TEXT', value: 'TEXT' },
+          { displayName: 'Hexadecimal', value: 'HEX' },
         ],
       },
       {
@@ -31,21 +33,21 @@ module.exports.templateTags = [
         placeholder: 'HMAC key used to generate signature',
       },
       {
-        displayName: 'HMAC Key Encoding',
+        displayName: 'HMAC Key Type',
         type: 'enum',
         options: [
-          { displayName: 'Hexadecimal', value: 'hex' },
-          { displayName: 'Base64', value: 'base64' },
+          { displayName: 'Base64', value: 'B64' },
+          { displayName: 'TEXT', value: 'TEXT' },
+          { displayName: 'Hexadecimal', value: 'HEX' },
         ],
       },
       {
         displayName: 'HMAC Signature Algorithm',
         type: 'enum',
         options: [
-          { displayName: 'MD5', value: 'md5' },
-          { displayName: 'SHA1', value: 'sha1' },
-          { displayName: 'SHA256', value: 'sha256' },
-          { displayName: 'SHA512', value: 'sha512' },
+          { displayName: 'SHA-1', value: 'SHA-1' },
+          { displayName: 'SHA-256', value: 'SHA-256' },
+          { displayName: 'SHA-512', value: 'SHA-512' },
         ],
       },
       {
@@ -53,8 +55,8 @@ module.exports.templateTags = [
         description: 'The encoding of the output',
         type: 'enum',
         options: [
-          { displayName: 'Hexadecimal', value: 'hex' },
-          { displayName: 'Base64', value: 'base64' },
+          { displayName: 'Base64', value: 'B64' },
+          { displayName: 'Hexadecimal', value: 'HEX' },
         ],
       },// digestPattern, value
       {
@@ -69,38 +71,56 @@ module.exports.templateTags = [
         placeholder: 'Value to hash (Otherwise body will be used)',
       },
     ],
-    run(context, hash_algorithm, hash_encoding, hmac_key, hmac_key_encoding, hmac_signature_algorithm, hmac_signature_encoding, digestPattern, digestPatternvalue = '') {
+    run(context, hash_algorithm, hash_encoding, hmac_key, hmac_key_encoding, hmac_signature_algorithm, hmac_signature_encoding, digestPattern, content = '') {
       const encodedUri = ''; //encodeURIComponent(context.request.getUrl());
       let hash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
-      const valueType = typeof value;
-      if (valueType !== 'string') {
-        throw new Error(`Cannot hash value of type "${valueType}"`);
+      console.log('content', `"${content}"`);
+
+      const contentType = typeof content;
+      if (contentType !== 'string') {
+        throw new Error(`Cannot hash content of type "${contentType}"`);
       }
 
-      if(digestPattern !== ''){
-        throw new Error(`Cannot make the signature with a DigestPattern value of "${digestPattern}"`);
+      if(digestPattern === ''){
+        throw new Error(`DigestPattern is not defined`);
       }
 
-      value = value || context.request.getBodyText() || '';
+      console.log('content before', `"${content}"`);
+      content = content || ''; // context.request.getBodyText() || '';
+      console.log('content after', `"${content}"`);
 
-      if(value !== ''){
-        const hash_builder = crypto.createHash(hash_algorithm);
-        hash_builder.update(value, 'utf8');
-        hash = hash_builder.digest(hash_encoding);
+      if(content !== ''){
+        // const hash_builder = crypto.createHash(hash_algorithm);
+        // hash_builder.update(content, 'utf8');
+        // hash = hash_builder.digest(hash_encoding);
+        var shaObj = new jsSHA(hash_algorithm, "TEXT");
+        shaObj.update(content);
+        hash = shaObj.getHash(hash_encoding);
       }
 
-      const digest = digestPattern.toLowerCase()
+      console.log('hash', hash);
+      console.log('digestPattern', digestPattern);
+
+      const digest = encodeURIComponent(digestPattern
           .replace(/{encodedUri}/g, encodedUri)
-          .replace(/{hash}/g, hash);
+          .replace(/{hash}/g, hash)).toLowerCase();
           //.split('{hash}').join(hash);
 
+      console.log('digest', digest);
       //(apiKey + nonce + timeStamp + config.method + encodeURIComponent(config.url) + dataHash).toLowerCase();
 
-      var hmacBuilder = new jsSHA(hmac_signature_algorithm, "utf8");
-      hmacBuilder.setHMACKey(hmac_key, hmac_key_encoding);
-      hmacBuilder.update(digest);
-      return hmacBuilder.getHMAC(hmac_signature_encoding);
+      // var hmacBuilder = crypto.createHmac(hmac_signature_algorithm, hmac_key, {decodeStrings: true, encoding: hmac_key_encoding});
+      // hmacBuilder.update(digest, 'utf8');
+      // const signature = hmacBuilder.digest(hmac_signature_encoding);
+      const hmacObj = new jsSHA(hmac_signature_algorithm, "TEXT");
+      hmacObj.setHMACKey(hmac_key, hmac_key_encoding);
+      hmacObj.update(digest);
+      const signature = hmacObj.getHMAC(hmac_signature_encoding);
+
+      console.log('signature', signature);
+      return signature;
+      // hmacBuilder.setHMACKey(hmac_key, hmac_key_encoding);
     },
   },
 ];
